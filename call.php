5b2c7a32ef823ce4ca735941cda6ea1967af8ab1
homesecures <68798130+homesecures@users.noreply.github.com>
2020-10-06 22:32:13 -0300

    Add files via upload

---
+++ call.php
@@ -0,0 +1,7 @@
+<?php
+$redirect[1] = 'https://callpages.com/home-security/all-regions-1/3b.php';
+$redirect['share'][1] = '50';
+$redirect[2] = 'https://callpages.com/home-security/all-regions-1/2b.php';
+$redirect['share'][2] = '50';
+require_once('mvlab.php');
+?>
